47acad6d8f45095f082571061a06c1a8c077bf03
Julio Cesar Dominguez Giraldo <juliodominguez@bloomtec.com>
2011-06-29 16:36:55 -0500

    modelo cambiado a utf8_general

--- mer/riviera.sql
+++ mer/riviera.sql
@@ -3,7 +3,7 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
 DROP SCHEMA IF EXISTS `riviera` ;
-CREATE SCHEMA IF NOT EXISTS `riviera` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+CREATE SCHEMA IF NOT EXISTS `riviera` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
 USE `riviera` ;
 
 -- -----------------------------------------------------
